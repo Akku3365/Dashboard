1172efe04711e89302c7fcd10f44c162b9ae1b30
Jemin Limbani <jamin.limbani@rentechdigital.com>
2023-07-25 14:25:32 +0530

    Editable Dashboard

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -1,63 +1,121 @@
-import React, { useContext, useEffect, useState } from 'react';
+/** @format */
+import React, { useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.css";
 
-
 function Dashboard(props) {
+    const initialInput = {
+        t: "",
+        de: "",
+    };
 
-    let initialSubmit;
+    // All States
+    const [tasks, setTasks] = useState(initialInput);
+    // const [description, setDescription] = useState('');
+    const [taskList, setTaskList] = useState([]);
+    const [editTasks, setEditTasks] = useState({});
+    const [editdTask, setEditdTask] = useState({ id: 0, t: "", de: "" });
 
-    // if (localStorage.getItem("Task") === null) {
-    //     initialSubmit = []
-    // } else {
-    //     initialSubmit = JSON.parse(localStorage.getItem("Task"))
-    // }
-
-    
-    const [submitTask, setSubmitTask] = useState(initialSubmit);
     useEffect(() => {
-        localStorage.setItem("Task", JSON.stringify(submitTask))
-    }, [submitTask])
-    
-    const initialTask = {
-        task: "",
-        desc: ""
-    }
-    const [tasks, setTasks] = useState(initialTask);
+        const storedItems = localStorage.getItem("Task");
+        if (storedItems) {
+            setTaskList(JSON.parse(storedItems));
+        }
+    }, []);
 
-    const taskInputChange = (e) => {
+    const handOnchangeFn = (e) => {
         const { name, value } = e.target;
         setTasks({ ...tasks, [name]: value });
-        console.log(tasks)
-    }
+    };
 
     const taskInputSubmit = () => {
-        setSubmitTask({ ...submitTask, tasks})
-    }
+        let temp_arr = [...taskList, { ...tasks, id: taskList.length + 1 }];
+        setTaskList(temp_arr);
+        localStorage.setItem("Task", JSON.stringify(temp_arr));
+        setTasks(initialInput);
+    };
 
-    console.log(submitTask)
+    const taskInputDelete = (i) => {
+        console.log(i);
+        let new_Arr = taskList.filter((task) => task.id !== i);
+        setTaskList(new_Arr);
+        localStorage.setItem("Task", JSON.stringify(new_Arr));
+    };
 
-    // console.log(props)
+    const taskEditFunction = (val) => {
+        setEditTasks(val);
+        setEditdTask(val)
+    };
+
+    // Update the input fields for editing the task
+    const handleEditChange = (e) => {
+        const { name, value } = e.target;
+        setEditdTask({ ...editdTask, [name]: value });
+    };
+
+    // Save the edited task
+
+    const handleSaveEdit = () => {
+        const taskIndex = taskList.findIndex((task) => task.id === editTasks.id);
+        const updatedtaskList = [...taskList];
+        if (taskIndex !== -1) {
+            updatedtaskList[taskIndex] = { ...editdTask, id: editTasks.id };
+        }
+        setTaskList(updatedtaskList);
+        localStorage.setItem("Task", JSON.stringify(updatedtaskList));
+        setEditdTask({
+            id: 0,
+            t: "",
+            de: "",
+        });
+        setEditTasks({});
+    };
 
     const backFunction = useNavigate();
 
     const handleback = () => {
         backFunction("/");
-    }
+    };
 
     return (
         <>
-            <div className='container'>
-                <h1 className='text-primary text-center'>Dashboard{props.role}</h1>
-                <label htmlFor='task mt-3'>Tasks</label>
-                <input type='text' name="task" value={tasks.task} onChange={taskInputChange} placeholder='Enter tasks'></input>
-                <input type='text' name="desc" value={tasks.desc} onChange={taskInputChange} placeholder='Description'></input>
-                <button onClick={taskInputSubmit} >Add Task</button>
-                <br />
-                <button className='mt-5' onClick={handleback} >Back to Home</button>
+            <div className="container">
+                <div>
+                    <h1 className="text-primary text-center">Dashboard{props.role}</h1>
+                    <label htmlFor="task">Tasks</label>
+                    <input type="text" value={tasks.t} name="t" onChange={handOnchangeFn} placeholder="Enter tasks"></input>
+                    <label htmlFor="task">Description</label>
+                    <input type="text" value={tasks.de} name="de" onChange={handOnchangeFn} placeholder="Description"></input>
+                    <button className="btn btn-dark" onClick={taskInputSubmit}>Add Task</button><br />
+                    <button className="mt-2" onClick={handleback}>
+                        Back to Home
+                    </button>
+                    <br />
+                </div>
+
+                <div>
+                    <ol className="list-group">
+                        {taskList.map((taskItem, i) => (
+                            <li className="list-group-item" key={i}>
+                                <p><span className="fs-4">Task:</span> {taskItem.t}</p>
+                                <p><span className="fs-4">Description:</span> {taskItem.de}</p>
+                                <span><button onClick={() => taskEditFunction(taskItem)} >Edit Task</button></span>
+                                <span><button onClick={() => taskInputDelete(taskItem.id)}>Delete task</button></span>
+                            </li>
+                        ))}
+                    </ol>
+                </div>
+            {editTasks.id !== undefined && (
+                <div>
+                    <h3>Edit Your Task</h3>
+                    <input type="text" name="t" value={editdTask.t} onChange={handleEditChange} placeholder="Edit your task"></input>
+                    <input type="text" name="de" value={editdTask.de} onChange={handleEditChange} placeholder="Edit your description"></input>
+                    <button onClick={handleSaveEdit}>Update</button>
+                </div>
+            )}
             </div>
         </>
-    )
+    );
 }
 
-export default Dashboard
+export default Dashboard;
