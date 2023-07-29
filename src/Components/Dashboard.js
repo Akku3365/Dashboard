ce5cc3be6d73e29962181c5553f2c56d89ee4281
Akash Srivastava <akashshrivastava00952@gmail.com>
2023-07-29 23:52:09 +0530

    both drag drop working

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -1,4 +1,194 @@
-/** @format */
+// /** @format */
+
+// import React, { useState, useEffect } from "react";
+// import { useNavigate } from "react-router-dom";
+// import "bootstrap/dist/css/bootstrap.css";
+// import Modal from "react-modal";
+// import { DragDropContext, Droppable } from "react-beautiful-dnd";
+// import lodashDebounce from "lodash.debounce";
+// import TaskForm from "./TaskForm";
+// import AnswerModal from "./AnswerModal";
+// import TaskList from "./TaskList"; // Import the new component
+// import Process from "./Process";
+// import Completed from "./Completed";
+
+// Modal.setAppElement("#root");
+
+// function Dashboard(props) {
+//     const { role } = props;
+
+//     const initialInput = {
+//         t: "",
+//         de: "",
+//         answer: "",
+//     };
+
+//     const [tasks, setTasks] = useState(initialInput);
+//     const [taskList, setTaskList] = useState(() => {
+//         const storedItems = localStorage.getItem("Task");
+//         return storedItems ? JSON.parse(storedItems) : [];
+//     });
+//     const [editTasks, setEditTasks] = useState(null);
+//     const [modalIsOpen, setModalIsOpen] = useState(false);
+//     const [currentTaskId, setCurrentTaskId] = useState(null);
+
+//     useEffect(() => {
+//         localStorage.setItem("Task", JSON.stringify(taskList));
+//     }, [taskList]);
+
+//     const handOnchangeFn = (e) => {
+//         const { name, value } = e.target;
+//         setTasks({ ...tasks, [name]: value });
+//     };
+
+//     const taskInputSubmit = () => {
+//         if (editTasks !== null) {
+//             const taskIndex = taskList.findIndex((task) => task.id === editTasks.id);
+//             const updatedtaskList = [...taskList];
+//             if (taskIndex !== -1) {
+//                 updatedtaskList[taskIndex] = { ...tasks, id: editTasks.id };
+//             }
+//             setTaskList(updatedtaskList);
+//             setEditTasks(null);
+//         } else {
+//             let temp_arr = [...taskList, { ...tasks, id: taskList.length }];
+//             setTaskList(temp_arr);
+//         }
+//         setTasks(initialInput);
+//     };
+
+//     const taskInputDelete = (id) => {
+//         setTaskList((prevList) => prevList.filter((task) => task.id !== id));
+//     };
+
+//     const taskEditFunction = (val) => {
+//         setEditTasks(val);
+//         setTasks(val);
+//     };
+
+//     const debouncedUpdateLocalStorage = lodashDebounce((updatedTasks) => {
+//         localStorage.setItem("Task", JSON.stringify(updatedTasks));
+//     }, 500);
+
+//     const handleDragEnd = (result) => {
+//         if (!result.destination) return;
+
+//         const updatedTasks = Array.from(taskList);
+//         const [removed] = updatedTasks.splice(result.source.index, 1);
+//         updatedTasks.splice(result.destination.index, 0, removed);
+
+//         setTaskList(updatedTasks);
+//         debouncedUpdateLocalStorage(updatedTasks);
+//     };
+
+//     const backFunction = useNavigate();
+
+//     const handleBack = () => {
+//         localStorage.removeItem("user_d");
+//         backFunction("/");
+//     };
+
+//     const openModal = (taskId) => {
+//         setCurrentTaskId(taskId);
+//         setModalIsOpen(true);
+//         const taskIndex = taskList.findIndex((task) => task.id === taskId);
+//         if (taskIndex !== -1) {
+//             setTasks(taskList[taskIndex]);
+//         }
+//     };
+
+//     const closeModal = () => {
+//         setCurrentTaskId(null);
+//         setModalIsOpen(false);
+//         setTasks(initialInput);
+//     };
+
+//     const submitAnswer = (event) => {
+//         event.preventDefault();
+//         if (currentTaskId !== null) {
+//             const taskIndex = taskList.findIndex((task) => task.id === currentTaskId);
+//             if (taskIndex !== -1) {
+//                 const updatedTaskList = [...taskList];
+//                 updatedTaskList[taskIndex] = { ...updatedTaskList[taskIndex], answer: tasks.answer };
+//                 setTaskList(updatedTaskList);
+//                 setModalIsOpen(false);
+//                 setTasks(initialInput);
+//             }
+//         }
+//     };
+
+//     return (
+//         <>
+//             <div className="container">
+//                 <div>
+//                     <h1 className="text-primary text-center">Dashboard</h1>
+//                     {role ? <h3 className="text-center">Welcome Teacher</h3> : <h3 className="text-center">Hello Student</h3>}
+//                     <div className="mt-4">{role && <TaskForm tasks={tasks} handOnchangeFn={handOnchangeFn} taskInputSubmit={taskInputSubmit} editTasks={editTasks} />}</div>
+//                     <br />
+//                     <button className="btn btn-primary mt-2" onClick={handleBack}>
+//                         Logout
+//                     </button>
+//                     <br />
+//                 </div>
+
+//                 <div className="d-flex flex-row mt-2">
+//                     <div>
+//                         <DragDropContext onDragEnd={handleDragEnd}>
+//                             <Droppable droppableId="task-list" isDropDisabled={!role}>
+//                                 {(provided) => (
+//                                     <div ref={provided.innerRef} {...provided.droppableProps}>
+//                                         <TaskList taskList={taskList} role={role} taskEditFunction={taskEditFunction} taskInputDelete={taskInputDelete} openModal={openModal} />
+//                                         {provided.placeholder}
+//                                     </div>
+//                                 )}
+//                             </Droppable>
+//                         </DragDropContext>
+//                     </div>
+
+//                     <div >
+//                         <DragDropContext onDragEnd={handleDragEnd}>
+//                             <Droppable droppableId="task-list" isDropDisabled={!role}>
+//                                 {(provided) => (
+//                                     <div ref={provided.innerRef} {...provided.droppableProps}>
+//                                         <Process taskList={taskList} role={role} taskEditFunction={taskEditFunction} taskInputDelete={taskInputDelete} openModal={openModal} />
+//                                         {provided.placeholder}
+//                                     </div>
+//                                 )}
+//                             </Droppable>
+//                         </DragDropContext>
+//                     </div>
+
+//                     <div>
+//                         <DragDropContext onDragEnd={handleDragEnd}>
+//                             <Droppable droppableId="task-list" isDropDisabled={!role}>
+//                                 {(provided) => (
+//                                     <div ref={provided.innerRef} {...provided.droppableProps}>
+//                                         <Completed taskList={taskList} role={role} taskEditFunction={taskEditFunction} taskInputDelete={taskInputDelete} openModal={openModal} />
+//                                         {provided.placeholder}
+//                                     </div>
+//                                 )}
+//                             </Droppable>
+//                         </DragDropContext>
+//                     </div>
+//                 </div>
+//             </div>
+
+//             <AnswerModal modalIsOpen={modalIsOpen} closeModal={closeModal} tasks={tasks} handOnchangeFn={handOnchangeFn} submitAnswer={submitAnswer} editTasks={editTasks} />
+//         </>
+//     );
+// }
+
+// export default Dashboard;
+
+
+
+
+
+
+
+
+
+
 
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
@@ -8,7 +198,7 @@ import { DragDropContext, Droppable } from "react-beautiful-dnd";
 import lodashDebounce from "lodash.debounce";
 import TaskForm from "./TaskForm";
 import AnswerModal from "./AnswerModal";
-import TaskList from "./TaskList"; // Import the new component
+import TaskList from "./TaskList";
 import Process from "./Process";
 import Completed from "./Completed";
 
@@ -21,6 +211,7 @@ function Dashboard(props) {
         t: "",
         de: "",
         answer: "",
+        status: "",
     };
 
     const [tasks, setTasks] = useState(initialInput);
@@ -73,12 +264,31 @@ function Dashboard(props) {
     const handleDragEnd = (result) => {
         if (!result.destination) return;
 
-        const updatedTasks = Array.from(taskList);
-        const [removed] = updatedTasks.splice(result.source.index, 1);
-        updatedTasks.splice(result.destination.index, 0, removed);
+        const sourceIndex = result.source.index;
+        const destinationIndex = result.destination.index;
+        const sourceListId = result.source.droppableId;
+        const destinationListId = result.destination.droppableId;
 
-        setTaskList(updatedTasks);
-        debouncedUpdateLocalStorage(updatedTasks);
+        if (sourceListId === destinationListId) {
+            // Dragged within the same list (taskList, process, completed)
+            const updatedTasks = Array.from(taskList);
+            const [removed] = updatedTasks.splice(sourceIndex, 1);
+            updatedTasks.splice(destinationIndex, 0, removed);
+
+            setTaskList(updatedTasks);
+            debouncedUpdateLocalStorage(updatedTasks);
+        } else {
+            // Dragged from taskList to Process or Completed
+            const taskToMove = taskList[sourceIndex];
+            taskToMove.status = destinationListId === "in-process" ? "In Process" : "Completed";
+
+            const updatedTasks = Array.from(taskList);
+            updatedTasks.splice(sourceIndex, 1);
+            updatedTasks.splice(destinationIndex, 0, taskToMove);
+
+            setTaskList(updatedTasks);
+            debouncedUpdateLocalStorage(updatedTasks);
+        }
     };
 
     const backFunction = useNavigate();
@@ -131,46 +341,48 @@ function Dashboard(props) {
                     <br />
                 </div>
 
-                <div className="d-flex flex-row">
-                    <div style={{width: "100%"}}>
-                        <DragDropContext onDragEnd={handleDragEnd}>
-                            <Droppable droppableId="task-list" isDropDisabled={!role}>
-                                {(provided) => (
-                                    <div ref={provided.innerRef} {...provided.droppableProps}>
-                                        <TaskList taskList={taskList} role={role} taskEditFunction={taskEditFunction} taskInputDelete={taskInputDelete} openModal={openModal} />
-                                        {provided.placeholder}
-                                    </div>
-                                )}
-                            </Droppable>
-                        </DragDropContext>
-                    </div>
+                {/* Drag and drop context */}
+                <DragDropContext onDragEnd={handleDragEnd}>
+                    <div className="d-flex flex-row mt-2">
+                        {/* Droppable for taskList */}
+                        <Droppable droppableId="task-list" isDropDisabled={!role}>
+                            {(provided) => (
+                                <div ref={provided.innerRef} {...provided.droppableProps}>
+                                    <TaskList taskList={taskList.filter((task) => !task.status)} role={role} taskEditFunction={taskEditFunction} taskInputDelete={taskInputDelete} openModal={openModal} />
+                                    {provided.placeholder}
+                                </div>
+                            )}
+                        </Droppable>
 
-                    <div className="col-md-4" >
-                        <DragDropContext onDragEnd={handleDragEnd}>
-                            <Droppable droppableId="task-list" isDropDisabled={!role}>
-                                {(provided) => (
-                                    <div ref={provided.innerRef} {...provided.droppableProps}>
-                                        <Process taskList={taskList} role={role} taskEditFunction={taskEditFunction} taskInputDelete={taskInputDelete} openModal={openModal} />
-                                        {provided.placeholder}
-                                    </div>
-                                )}
-                            </Droppable>
-                        </DragDropContext>
-                    </div>
+                        {/* Droppable for Process */}
+                        <Droppable droppableId="in-process" isDropDisabled={!role}>
+                            {(provided) => (
+                                <div ref={provided.innerRef} {...provided.droppableProps}>
+                                    {taskList.some((task) => task.status === "In Process") ? (
+                                        <Process taskList={taskList.filter((task) => task.status === "In Process")} role={role} taskEditFunction={taskEditFunction} taskInputDelete={taskInputDelete} openModal={openModal} />
+                                    ) : (
+                                        <p className="text-center">In Process: No tasks yet</p>
+                                    )}
+                                    {provided.placeholder}
+                                </div>
+                            )}
+                        </Droppable>
 
-                    <div className="col-md-4">
-                        <DragDropContext onDragEnd={handleDragEnd}>
-                            <Droppable droppableId="task-list" isDropDisabled={!role}>
-                                {(provided) => (
-                                    <div ref={provided.innerRef} {...provided.droppableProps}>
-                                        <Completed taskList={taskList} role={role} taskEditFunction={taskEditFunction} taskInputDelete={taskInputDelete} openModal={openModal} />
-                                        {provided.placeholder}
-                                    </div>
-                                )}
-                            </Droppable>
-                        </DragDropContext>
+                        {/* Droppable for Completed */}
+                        <Droppable droppableId="completed" isDropDisabled={!role}>
+                            {(provided) => (
+                                <div ref={provided.innerRef} {...provided.droppableProps}>
+                                    {taskList.some((task) => task.status === "Completed") ? (
+                                        <Completed taskList={taskList.filter((task) => task.status === "Completed")} role={role} taskEditFunction={taskEditFunction} taskInputDelete={taskInputDelete} openModal={openModal} />
+                                    ) : (
+                                        <p className="text-center">Completed: No tasks yet</p>
+                                    )}
+                                    {provided.placeholder}
+                                </div>
+                            )}
+                        </Droppable>
                     </div>
-                </div>
+                </DragDropContext>
             </div>
 
             <AnswerModal modalIsOpen={modalIsOpen} closeModal={closeModal} tasks={tasks} handOnchangeFn={handOnchangeFn} submitAnswer={submitAnswer} editTasks={editTasks} />
@@ -179,3 +391,4 @@ function Dashboard(props) {
 }
 
 export default Dashboard;
+
--- src/Components/TaskList.js
+++ src/Components/TaskList.js
@@ -1,4 +1,69 @@
-/** @format */
+// /** @format */
+
+// import React from "react";
+// import { Draggable } from "react-beautiful-dnd";
+
+// function TaskList({ taskList, role, taskEditFunction, taskInputDelete, openModal }) {
+//     return (
+//         <>
+//         <div style={{border: "2px solid black", width: "100%"}} >
+//             <ol className="list-group">
+//             <h1 className="text-center">Task List</h1>
+//                 {taskList.map((taskItem, index) => (
+//                     <Draggable key={taskItem.id.toString()} draggableId={taskItem.id.toString()} index={index} isDragDisabled={!role}>
+//                         {(provided) => (
+//                             <li className="list-group-item mt-2" style={{ border: "2px solid lightskyblue", background: "floralwhite", borderRadius: "5px" }} ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+//                                 <p>
+//                                     <span className="fs-4">Task:</span> {taskItem.t}
+//                                 </p>
+//                                 <p>
+//                                     <span className="fs-4">Description:</span> {taskItem.de}
+//                                 </p>
+//                                 {taskItem.answer && (
+//                                     <p>
+//                                         <span className="fs-4 text-primary">Answer:</span> {taskItem.answer}
+//                                     </p>
+//                                 )}
+//                                 {role && (
+//                                     <>
+//                                         <span>
+//                                             <button className="btn btn-warning" onClick={() => taskEditFunction(taskItem)}>
+//                                                 Edit Task
+//                                             </button>
+//                                         </span>
+//                                         <span>
+//                                             <button className="btn btn-danger" onClick={() => taskInputDelete(taskItem.id)}>
+//                                                 Delete task
+//                                             </button>
+//                                         </span>
+//                                     </>
+//                                 )}
+//                                 {!role && !taskItem.answer && (
+//                                     <button className="btn btn-primary" onClick={() => openModal(taskItem.id)}>
+//                                         Answer
+//                                     </button>
+//                                 )}
+//                                 {!role && taskItem.answer && (
+//                                     <button className="btn btn-primary" onClick={() => openModal(taskItem.id)}>
+//                                         Edit Answer
+//                                     </button>
+//                                 )}
+//                             </li>
+//                         )}
+//                     </Draggable>
+//                 ))}
+//             </ol>
+//             </div>
+//         </>
+//     );
+// }
+
+// export default TaskList;
+
+
+
+
+
 
 import React from "react";
 import { Draggable } from "react-beautiful-dnd";
@@ -6,56 +71,57 @@ import { Draggable } from "react-beautiful-dnd";
 function TaskList({ taskList, role, taskEditFunction, taskInputDelete, openModal }) {
     return (
         <>
-        <div style={{border: "2px solid black", width: "32%"}} >
-            <ol className="list-group">
-            <h1 className="text-center">Task List</h1>
-                {taskList.map((taskItem, index) => (
-                    <Draggable key={taskItem.id.toString()} draggableId={taskItem.id.toString()} index={index} isDragDisabled={!role}>
-                        {(provided) => (
-                            <li className="list-group-item mt-2" style={{ border: "2px solid lightskyblue", background: "floralwhite", borderRadius: "5px" }} ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
-                                <p>
-                                    <span className="fs-4">Task:</span> {taskItem.t}
-                                </p>
-                                <p>
-                                    <span className="fs-4">Description:</span> {taskItem.de}
-                                </p>
-                                {taskItem.answer && (
+            <div style={{ border: "2px solid black", width: "100%" }}>
+                <ol className="list-group">
+                    <h1 className="text-center">Task List</h1>
+                    {taskList.map((taskItem, index) => (
+                        <Draggable key={taskItem.id.toString()} draggableId={taskItem.id.toString()} index={index} isDragDisabled={!role}>
+                            {(provided) => (
+                                <li className="list-group-item mt-2" style={{ border: "2px solid lightskyblue", background: "floralwhite", borderRadius: "5px" }} ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
                                     <p>
-                                        <span className="fs-4 text-primary">Answer:</span> {taskItem.answer}
+                                        <span className="fs-4">Task:</span> {taskItem.t}
                                     </p>
-                                )}
-                                {role && (
-                                    <>
-                                        <span>
-                                            <button className="btn btn-warning" onClick={() => taskEditFunction(taskItem)}>
-                                                Edit Task
-                                            </button>
-                                        </span>
-                                        <span>
-                                            <button className="btn btn-danger" onClick={() => taskInputDelete(taskItem.id)}>
-                                                Delete task
-                                            </button>
-                                        </span>
-                                    </>
-                                )}
-                                {!role && !taskItem.answer && (
-                                    <button className="btn btn-primary" onClick={() => openModal(taskItem.id)}>
-                                        Answer
-                                    </button>
-                                )}
-                                {!role && taskItem.answer && (
-                                    <button className="btn btn-primary" onClick={() => openModal(taskItem.id)}>
-                                        Edit Answer
-                                    </button>
-                                )}
-                            </li>
-                        )}
-                    </Draggable>
-                ))}
-            </ol>
+                                    <p>
+                                        <span className="fs-4">Description:</span> {taskItem.de}
+                                    </p>
+                                    {taskItem.status && (
+                                        <p>
+                                            <span className="fs-4 text-primary">Status:</span> {taskItem.status}
+                                        </p>
+                                    )}
+                                    {role && (
+                                        <>
+                                            <span>
+                                                <button className="btn btn-warning" onClick={() => taskEditFunction(taskItem)}>
+                                                    Edit Task
+                                                </button>
+                                            </span>
+                                            <span>
+                                                <button className="btn btn-danger" onClick={() => taskInputDelete(taskItem.id)}>
+                                                    Delete task
+                                                </button>
+                                            </span>
+                                        </>
+                                    )}
+                                    {!role && !taskItem.status && (
+                                        <button className="btn btn-primary" onClick={() => openModal(taskItem.id)}>
+                                            Answer
+                                        </button>
+                                    )}
+                                    {!role && taskItem.status && (
+                                        <button className="btn btn-primary" onClick={() => openModal(taskItem.id)}>
+                                            Edit Answer
+                                        </button>
+                                    )}
+                                </li>
+                            )}
+                        </Draggable>
+                    ))}
+                </ol>
             </div>
         </>
     );
 }
 
 export default TaskList;
+
